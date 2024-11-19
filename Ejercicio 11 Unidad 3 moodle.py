1288f2a345efde733d90fb913e20f6bdea4c115c
Alejandro Pérez Pérez <alexper1735@gmail.com>
2024-11-19 21:20:18 +0100

    Delete Ejercicio 11 Unidad 3 moodle.py

--- Ejercicio 11 Unidad 3 moodle.py
+++
@@ -1,8 +0,0 @@
-size = input('Enter the size of the square:')
-size = int(size)
-
-if size <= 0:
-    print('The size must be more bigger.')
-else:
-    for i in range(size):
-        print("*" * size)
